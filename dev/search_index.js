55a44a3b6c9230c11d722b5433d55400593cf469
Documenter.jl <documenter@juliadocs.github.io>
2022-02-21 10:38:50 +0000

    build based on 96945c6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EMSimJulia","category":"page"},{"location":"#EMSimJulia","page":"Home","title":"EMSimJulia","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EMSimJulia.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EMSimJulia]","category":"page"}]
+}
